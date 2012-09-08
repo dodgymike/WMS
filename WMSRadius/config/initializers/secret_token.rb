3c1791c306484b55c3366a97bc32c00a01ca9c47
Mike Davis <mike@lookhere.co.za>
2012-09-08 18:48:50 +0200

    - initial rails project for Radius config

---
+++ WMSRadius/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WMSRadius::Application.config.secret_token = '432f62da63daf9b166adc8c69aa78418910f82ef094ece7817a6277949b9d898d4e42fb656e0efa2c11b05fb5bb211ab95de1ed9f625bd0360d1cd2beb5c88b5'
