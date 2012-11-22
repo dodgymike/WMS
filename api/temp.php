8ebfe1826952f53207dc90f31a2b5bc1a6e9e460
Mike Davis <mike@lookhere.co.za>
2012-11-22 22:29:47 +0200

    - added temp.php

---
+++ api/temp.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * update.php
+ * this forms part of the CTWUG NOC
+ *
+ * Simply returns 'done'
+ *
+ * Mike Davis
+ * 2012/11/22
+*/
+
+//sample api call
+http://noc.ctwug.za.net/web/api/temp?id=FID&serial=SERIAL
+
+echo 'done';
